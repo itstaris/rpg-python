aa4a1cd0166a90928638a5120a7d12a790c9b215
Tamires <tamires.ei04@gmail.com>
2024-11-06 13:25:42 -0300

    DEU CERTO (OU QUASE)

--- teste_de_leitura.py
+++ teste_de_leitura.py
@@ -3,8 +3,9 @@ meuArquivo = open("introdução.txt", "r", encoding="utf-8")
 
 minhasLinhasDeTexto = meuArquivo.readlines()
 
-def imprimeLista(index):
+#def imprimeLista(index):
     
-    return minhasLinhasDeTexto[index]
+   # return minhasLinhasDeTexto[index]
 
-
+contador = 1
+print(minhasLinhasDeTexto[contador])
